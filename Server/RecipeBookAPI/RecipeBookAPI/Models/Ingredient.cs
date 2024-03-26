e0f9cda9bb573df900db133f506970b102491be0
Shawn <yxzhang2@illinois.edu>
2024-03-26 10:24:39 -0400

    fix(backend):Fixed folder structure of project

--- Server/RecipeBookAPI/RecipeBookAPI/Models/Ingredient.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace RecipeBookAPI.Models
-{
-    public class Ingredient
-    {
-        public string Name { get; set; }
-        public double Quantity { get; set; }
-        public string Unit { get; set; }
-    }
-}
---
+++ server/Models/Ingredient.cs
@@ -0,0 +1,9 @@
+﻿namespace server.Models
+{
+    public class Ingredient
+    {
+        public string Name { get; set; }
+        public double Quantity { get; set; }
+        public string Unit { get; set; }
+    }
+}
